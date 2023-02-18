0d9b4288b34c7b02703244c3e2bfc880447bfe25
EWhalen2 <emmett.whalen@kellogg.northwestern.edu>
2023-02-18 23:45:38 +0000

    Edits to places and posts controllers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,21 @@
 class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+      end
+    
+      def show
+        @place = Place.find_by({ "id" => params["id"] })
+        @place = Place.where({ "place_id" => @place["name"] })
+      end
+    
+      def new
+        @place = Place.new
+      end
+    
+      def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
 end
