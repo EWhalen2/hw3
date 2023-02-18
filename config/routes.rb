d008986045fa18f6baa2f21595cf6703d84cc0ba
EWhalen2 <emmett.whalen@kellogg.northwestern.edu>
2023-02-18 16:44:05 +0000

    defined posts and places index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources "places"
   resources "posts"
+def index
+  @posts = Post.all
+end
+
+def index
+  @places = Place.all
+end
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
