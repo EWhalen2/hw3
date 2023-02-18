3d198d933b31ac8e8952554f8e542f82928ed76a
EWhalen2 <emmett.whalen@kellogg.northwestern.edu>
2023-02-18 23:03:32 +0000

    built post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+    def new
+        @post = Post.new
+    end
 end
+
+<%= form_with :model @post do |post| %>
+    <p>
+    <%= form.label "post", "New Post" %>
+    <%= form.text_field "New Post" %>
+    </p>
+    
+    <p>
+    <%= form.label "title" %>
+    <%= form.text_field "title" %>
+    <%= form.label "description" %>
+    <%= form.text_field "description" %>
+    <%= form.label "date" %>
+    <%= form.text_field "date" %>
+    </p>
+    <%= form.submit %>
+    <% end %>
