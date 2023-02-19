ba5ccfeb15680b6552e221bf53eb1c56ab548bc5
EWhalen2 <emmett.whalen@kellogg.northwestern.edu>
2023-02-19 00:44:05 +0000

    Final save for the night

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
       def new
         @post = Post.new
         @place = Place.find_by({ "id" => params["id"]})
-        @post["place_id"] = @place["id"]
+       # @post["place_id"] = @place["id"]
       end
     
       def create
