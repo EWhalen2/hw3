763961ae3beeef5891bb11860192600fa44b363b
EWhalen2 <emmett.whalen@kellogg.northwestern.edu>
2023-02-20 01:23:51 +0000

    Successfully posted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,8 @@ class PostsController < ApplicationController
     
       def new
         @post = Post.new
-        @place = Place.find_by({ "id" => params["id"]})
-       # @post["place_id"] = @place["id"]
+        @place = Place.find_by({ "id" => params["place_id"]})
+        @post["place_id"] = @place["id"]
       end
     
       def create
